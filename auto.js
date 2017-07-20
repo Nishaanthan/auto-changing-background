bddbde25144bba87b658095856ad639165f893ef
Nisanthan <nisanthan@email.com>
2017-07-20 18:26:48 +0530

    Third Commit

--- auto.js
+++ auto.js
@@ -1,1 +1,12 @@
-vbvcisfhksdhfdf 
+var script = document.createElement('script');
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js";
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+var url = "https://api.desktoppr.co/1/wallpapers/random";
+$.getJSON(url,function (err,data) {
+    if(err!=unll){
+        window.alert('something went wrong : '+err);
+    }else{
+        alert('Your query count : '+data.query.count);
+    }
+});
